207725330f39b6b5713de61151b95a4e2f0fcbfb
iyiu9akhan <iyiu9akhan@gmail.com>
2026-09-12 04:02:27 +0600

    technologies section's ui design: completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@
 
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Technologies from "../components/Technologies";
 
 function Home() {
   return (
     <>
       <Header />
       <Banner/>
+      <Technologies/>
     </>
   );
 }
